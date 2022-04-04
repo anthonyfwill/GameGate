8e7ae130c6d5b708abb37e9670a6bd74425f618d
Robert Bangiyev <rbangiyev123@gmail.com>
2022-04-03 21:23:53 -0400

    Added settings routing, fixed naming

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -4,7 +4,7 @@ export default function SearchResult(props) {
     return (
         <div className="search-result-container">
             <img src={props.img} alt={props.name} />
-            <p>{props.name} {props.year && (props.year)}</p>
+            <p>{props.name} {props.year && '(' + props.year + ')'}</p>
         </div>
     )
 }
