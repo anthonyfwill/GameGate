57177c0ce411436771c37edbcb66a73a9cf38e5b
Mohammad Sarker <sarkerm948@gmail.com>
2022-05-10 15:45:04 -0400

    updated landing page format

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,11 +2,17 @@ import React from "react";
 
 function LandingPage() {
     return (
-        <div className="indexcaptions">
-            <h1>Log your gaming history using our expansive database and user-friendly interface.</h1>
-            <h1>Rate your favorites and compare them with other users across the site.</h1>
-            <h1>Review and share your opinions whilst making new friends. </h1>
+        <div className="captions">
+            <img src="https://i.imgur.com/Auyx11D.png" alt="img1" height="500" width="905" />
+            <h2>Log your gaming history using our expansive database and user-friendly interface.</h2>
+
+            <img src="https://i.imgur.com/S9kxGWS.png" alt="img2" height="500" width="905" />
+            <h2>Rate your favorites and compare them with other users across the site.</h2>
+
+            <img src="https://i.imgur.com/6EDLvjk.png" alt="img3" height="500" width="905" />
+            <h2>Review and share your opinions whilst making new friends.</h2>
         </div>
+
     )
 }
 
