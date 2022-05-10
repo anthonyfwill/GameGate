a6e17216bfc3e90af22a7bcaf41a17451b779df0
Mohammad Sarker <sarkerm948@gmail.com>
2022-05-10 15:57:00 -0400

    landing page heading

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,16 +2,18 @@ import React from "react";
 
 function LandingPage() {
     return (
-        <div className="captions">
-            <img src="https://i.imgur.com/Auyx11D.png" alt="img1" height="500" width="905" />
-            <h2>Log your gaming history using our expansive database and user-friendly interface.</h2>
+        <><div className="indexcaptions">
+            <h1>GameGate</h1>
+        </div><div className="captions">
+                <img src="https://i.imgur.com/Auyx11D.png" alt="img1" height="500" width="905" />
+                <h2>Log your gaming history using our expansive database and user-friendly interface.</h2>
 
-            <img src="https://i.imgur.com/S9kxGWS.png" alt="img2" height="500" width="905" />
-            <h2>Rate your favorites and compare them with other users across the site.</h2>
+                <img src="https://i.imgur.com/S9kxGWS.png" alt="img2" height="500" width="905" />
+                <h2>Rate your favorites and compare them with other users across the site.</h2>
 
-            <img src="https://i.imgur.com/6EDLvjk.png" alt="img3" height="500" width="905" />
-            <h2>Review and share your opinions whilst making new friends.</h2>
-        </div>
+                <img src="https://i.imgur.com/6EDLvjk.png" alt="img3" height="500" width="905" />
+                <h2>Review and share your opinions whilst making new friends.</h2>
+            </div></>
 
     )
 }
