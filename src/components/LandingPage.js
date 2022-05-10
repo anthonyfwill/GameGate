b3f8d73339d2eb84c5e286d05e6764b75ba94694
Mohammad Sarker <sarkerm948@gmail.com>
2022-05-10 17:11:01 -0400

    image bug

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,14 +4,15 @@ function LandingPage() {
     return (
         <><div className="indexcaptions">
             <h1>GameGate</h1>
-        </div><div className="captions">
-                <img src="https://i.imgur.com/Auyx11D.png" alt="img1" height="500" width="905" />
+        </div>
+        <div className="captions">
+                <img className="caption_images" src="https://i.imgur.com/Auyx11D.png" alt="img1" height="500" width="905" />
                 <h2>Log your gaming history using our expansive database and user-friendly interface.</h2>
 
-                <img src="https://i.imgur.com/S9kxGWS.png" alt="img2" height="500" width="905" />
+                <img className="caption_images" src="https://i.imgur.com/S9kxGWS.png" alt="img2" height="500" width="905" />
                 <h2>Rate your favorites and compare them with other users across the site.</h2>
 
-                <img src="https://i.imgur.com/6EDLvjk.png" alt="img3" height="500" width="905" />
+                <img className="caption_images" src="https://i.imgur.com/6EDLvjk.png" alt="img3" height="500" width="905" />
                 <h2>Review and share your opinions whilst making new friends.</h2>
             </div></>
 
