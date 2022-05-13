7f01ba04cd54c3fe4e0c84eb6e1abcf7fdec4250
Robert Bangiyev <rbangiyev123@gmail.com>
2022-05-13 04:16:06 -0400

    Moved registration to backend

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,17 +1,5 @@
-import * as AWS from 'aws-sdk';
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import UserPool from './UserPool';
-
-
-// var myCredentials = new AWS.CognitoIdentityCredentials({IdentityPoolId:'us-east-1:1f1634e0-e85f-4ffe-a509-ecb75c777309'});
-// var myConfig = new AWS.Config({
-//   credentials: myCredentials, region: 'us-east-1'
-// });
-
-// AWS.config.update(myConfig)
-
-// const docClient = new AWS.DynamoDB.DocumentClient()
 
 const Register = (props) => {
 
@@ -23,108 +11,40 @@ const Register = (props) => {
     const [error, setError] = useState(null);
 
     const signUp = (email, pw, username) => {
-        var params = {
-            TableName: "GameGateAccounts",
-            KeyConditionExpression: "#email = :email3",
-            ExpressionAttributeNames: {
-                "#email": "Email"
-            },
-            ExpressionAttributeValues: {
-                ":email3": email
-            }
-        }
-        var canMake = 0;
+        if(password !== confirmPassword) {
+            setError('Passwords not equal');
+        } else if(!email || !pw || !username) {
+            setError('One of more fields is empty');
+        } else {
+            var myHeaders = new Headers();
+            myHeaders.append("Content", "application/json");
+            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-        props.docClient.query(params, function(err, data) {
-            if (!err) {
-                //console.log("no error");
-                //console.log(data, "Email entered: " + email);
-                if (data.Count === 0) {
-                    console.log("Email available");
-                    canMake = canMake + 1;
+            var urlencoded = new URLSearchParams();
+            urlencoded.append("email", email);
+            urlencoded.append("pw", pw);
+            urlencoded.append("username", username);
+
+            var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: urlencoded,
+            redirect: 'follow'
+            };
+
+            fetch("http://localhost:5000/api/users", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                if(result.success)  {
+                    history.push(`/login`);
                 } else {
-                    console.log("Email is not available");
+                    setError('Registration failed');
                 }
-            } else {
-                canMake += 1
-                console.log(err);
-            }
-        })
-
-        var params3 = {
-            TableName: "GameGateAccounts",
-            IndexName: "Username-index",
-            KeyConditionExpression: "#username = :User3",
-            ExpressionAttributeNames: {
-                "#username": "Username"
-            },
-            ExpressionAttributeValues: {
-                ":User3": username
-            }
+            })
+            .catch(error => {
+                setError("Username or email already used");
+            })
         }
-
-        props.docClient.query(params3, function(err, data) {
-            if (!err) {
-                //console.log("no error");
-                //console.log(data.Count, "Username entered: " + username);
-                if (data.Count === 0) {
-                    console.log("Username is available");
-                    canMake = canMake + 1;
-                    console.log("canMake:", canMake);
-                    if (canMake === 2) {
-                        UserPool.signUp(email, pw, [], null, (err, data) => {
-                            if (err) {
-                                console.error(err);
-                                setError("Password must be at least 6 characters");
-                            } else {
-                                console.log(data);
-                                var params2 = {
-                                    TableName: "GameGateAccounts",
-                                    Item: {
-                                        "Email": email,
-                                        "Password": pw,
-                                        "Username": username,
-                                        "ProfilePicture": "https://i.imgur.com/y0B5yj6.jpg",
-                                        "CurrentG": 0,
-                                        "Completed": 0,
-                                        "Dropped": 0,
-                                        "Planning": 0,
-                                        "Followers": 0,
-                                        "Following": 0,
-                                        "FollowersMap": {},
-                                        "FollowingMap": {},
-                                        "CompletedGames": {},
-                                        "CurrentGames": {},
-                                        "DroppedGames": {},
-                                        "PlanningGames": {}
-                                    }
-                                }
-                                props.docClient.put(params2, function(err, data2) {
-                                    if (!err) {
-                                        console.log("Worked");
-                                        /*props.setCurrUserInfo(params2.Item);
-                                        props.setCurrUser(username);
-                                        props.setLoggedIn(true);
-                                        history.push(`/profile/${username}`);*/
-                                        history.push(`/login`);
-
-                                    } else {
-                                        console.log("Not Worked");
-                                        console.log(err);
-                                    }
-                                })
-                            }
-                        })
-                    }
-                } else {
-                    console.log("Username is not available");
-                    setError('Email or Username are already used');
-                }
-            } else {
-                //console.log(data.Username, "Username entered: " + username);
-                console.log(err);
-            }
-        })
     }
 
     return (
