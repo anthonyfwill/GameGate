ac1cd86a6f34518ef4018c5fbf0498ed61fcc8a4
anthonyfw <anthonyfletcherw@gmail.com>
2022-05-17 15:36:21 -0400

    Condition for username

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -183,115 +183,135 @@ const Settings = (props) => {
     }
 
     function updateUsername() {
-        var params = {
-            TableName:"GameGateAccounts",
-            Key:{
-                "Email": props.currUserInfo.Email
-            },
-            UpdateExpression: "set Username = :username",
-            ExpressionAttributeValues:{
-                ":username": newUsername
-            },
-            ReturnValues:"UPDATED_NEW"
-        };
-
-        props.docClient.update(params, function(err, data) {
-            if (err) {
-                // console.log(err);
-            } else {
-                const newResults = {};
-                const someVal = Object.assign(newResults, props.currUserInfo);
-                console.log(newResults);
-                newResults.Username = newUsername;
-                props.setCurrUserInfo(newResults);
-                localStorage.setItem('user', JSON.stringify(newResults));
-            }
-        });
-
-        var params2 = {
-            TableName: "Games",
-            IndexName: "Email-GameID-index",
-            KeyConditionExpression: "#email = :Email3",
+        var params5 = {
+            TableName: "GameGateAccounts",
+            IndexName: "Username-index",
+            KeyConditionExpression: "#username = :User3",
             ExpressionAttributeNames: {
-                "#email": "Email"
+                "#username": "Username"
             },
             ExpressionAttributeValues: {
-                ":Email3": props.currUserInfo.Email
+                ":User3": newUsername
             }
         }
-    
-        props.docClient.query(params2, function(err, data) {
-            if (!err) {
-                if (data.Count === 0) {
-                    // console.log(data);
-                } else {
-                    // console.log(data);
-                    data.Items.forEach(item => {
-                        console.log(item.GameID);
-                        console.log(props.currUserInfo.Email);
-                        var params1 = {
-                            TableName:"Games",
-                                Key:{
-                                "GameID": item.GameID,
-                                "Email": props.currUserInfo.Email
-                            },
-                            UpdateExpression: "set Username = :Username",
-                            ExpressionAttributeValues:{
-                                ":username":newUsername
-                            },
-                            ReturnValues:"UPDATED_NEW"
-                        };
-                        // console.log(item);
-                        props.docClient.update(params1, function(err, data) {
-                            if (err) {
-                                console.log(err);
-                                // console.log(err);
+        props.docClient.query(params5, function(err, data){
+            if(!err) {
+                console.log(data);
+                if (data.Count > 0) {
+                    //Display Username taken
+                    console.log("Username taken");
+                } else if (data.Count === 0) {
+                    var params = {
+                        TableName:"GameGateAccounts",
+                        Key:{
+                            "Email": props.currUserInfo.Email
+                        },
+                        UpdateExpression: "set Username = :username",
+                        ExpressionAttributeValues:{
+                            ":username": newUsername
+                        },
+                        ReturnValues:"UPDATED_NEW"
+                    };
+
+                    props.docClient.update(params, function(err, data) {
+                        if (err) {
+                            // console.log(err);
+                        } else {
+                            const newResults = {};
+                            const someVal = Object.assign(newResults, props.currUserInfo);
+                            console.log(newResults);
+                            newResults.Username = newUsername;
+                            props.setCurrUserInfo(newResults);
+                            localStorage.setItem('user', JSON.stringify(newResults));
+                        }
+                    });
+
+                    var params2 = {
+                        TableName: "Games",
+                        IndexName: "Email-GameID-index",
+                        KeyConditionExpression: "#email = :Email3",
+                        ExpressionAttributeNames: {
+                            "#email": "Email"
+                        },
+                        ExpressionAttributeValues: {
+                            ":Email3": props.currUserInfo.Email
+                        }
+                    }
+                
+                    props.docClient.query(params2, function(err, data) {
+                        if (!err) {
+                            if (data.Count === 0) {
+                                // console.log(data);
                             } else {
-                                console.log(data);
-                                // console.log("Updated the username of all reviews by", props.currUser);
+                                // console.log(data);
+                                data.Items.forEach(item => {
+                                    //console.log(item.GameID);
+                                    //console.log(props.currUserInfo.Email);
+                                    var params1 = {
+                                        TableName:"Games",
+                                            Key:{
+                                            "GameID": item.GameID,
+                                            "Email": props.currUserInfo.Email
+                                        },
+                                        UpdateExpression: "set Username = :username",
+                                        ExpressionAttributeValues:{
+                                            ":username": newUsername
+                                        },
+                                        ReturnValues:"UPDATED_NEW"
+                                    };
+                                    // console.log(item);
+                                    props.docClient.update(params1, function(err, data) {
+                                        if (err) {
+                                            console.log(err);
+                                            // console.log(err);
+                                        } else {
+                                            console.log(data);
+                                            // console.log("Updated the username of all reviews by", props.currUser);
+                                        }
+                                    });
+                                })
                             }
-                        });
+
+                        } else {
+                            // console.log(err);
+                        }
+                    })
+
+                    var params3 = {
+                        TableName: "GameGateAccounts",
+                        KeyConditionExpression: "#email = :Email3",
+                        ExpressionAttributeNames: {
+                            "#email": "Email"
+                        },
+                        ExpressionAttributeValues: {
+                            ":Email3": props.currUserInfo.Email
+                        }
+                    }
+                
+                    props.docClient.query(params3, function(err, data) {
+                        if (!err) {
+                            if (data.Count === 0) {
+                                // console.log(data);
+                            } else {
+                                // console.log(data);
+                                // console.log(data.Items[0].FollowingMap, "FollowingMap");
+                                for (var item2 in data.Items[0].FollowingMap) {
+                                    // console.log(item2, "each following");
+                                    usernameFollowersMap(item2);
+                                }
+                                for (var item2 in data.Items[0].FollowersMap) {
+                                    // console.log(item2, "each following");
+                                    usernameFollowingMap(item2);
+                                }
+                            }
+
+                        } else {
+                            // console.log(err);
+                        }
                     })
                 }
-
-            } else {
-                // console.log(err);
             }
-        })
-
-        var params3 = {
-            TableName: "GameGateAccounts",
-            KeyConditionExpression: "#email = :Email3",
-            ExpressionAttributeNames: {
-                "#email": "Email"
-            },
-            ExpressionAttributeValues: {
-                ":Email3": props.currUserInfo.Email
-            }
-        }
-    
-        props.docClient.query(params3, function(err, data) {
-            if (!err) {
-                if (data.Count === 0) {
-                    // console.log(data);
-                } else {
-                    // console.log(data);
-                    // console.log(data.Items[0].FollowingMap, "FollowingMap");
-                    for (var item2 in data.Items[0].FollowingMap) {
-                        // console.log(item2, "each following");
-                        usernameFollowersMap(item2);
-                    }
-                    for (var item2 in data.Items[0].FollowersMap) {
-                        // console.log(item2, "each following");
-                        usernameFollowingMap(item2);
-                    }
-                }
-
-            } else {
-                // console.log(err);
-            }
-        })
-        
+        })        
     }
 
     function usernameFollowersMap(item2){
